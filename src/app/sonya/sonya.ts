300b39995729aac0646995f1258e7cdd4fc457da
Katherine <katerynaunik@gmail.com>
2026-05-06 23:35:40 +0300

    fixed bugs

--- src/app/sonya/sonya.ts
+++ src/app/sonya/sonya.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterOutlet, NavigationEnd} from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-sonya',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './sonya.html',
   styleUrl: './sonya.css',
 })
@@ -11,12 +13,25 @@ export class Sonya {
 
   currentRoute = '';
 
-  constructor(public router: Router) {
-    this.router.events.subscribe(event =>
-      if(event instanceof NavigationEnd) {
-        this.currentRoute = this.router.url
-      }
-    )
-  }
+  constructor(public router: Router) {}
 
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      const url = event.urlAfterRedirects;
+      this.updateBackground(url);
+    });
+  }
+  updateBackground(url: string) {
+    if (url.includes('marinaHawaiiTravel')) {
+      this.currentRoute = 'bg-marina';
+    } else if (url.includes('howToGetToTheHawaii')) {
+      this.currentRoute = 'bg-cake';
+    } else if (url.includes('contacts')) {
+      this.currentRoute = 'bg-katya';
+    } else {
+      this.currentRoute = 'bg-olya';
+    }
+  }
 }
